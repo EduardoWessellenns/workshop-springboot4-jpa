379d0f1eab7095500c59ec93300c36bc3dbec728
Eduardo Wessellenns <eduardowessellennss@gmail.com>
2026-05-05 15:58:45 -0300

    many-to-many association with JoinTable

--- src/main/java/com/example/curso/entities/Category.java
+++ src/main/java/com/example/curso/entities/Category.java
@@ -1,5 +1,6 @@
 package com.example.curso.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -21,7 +22,9 @@ public class Category implements Serializable {
     }
 
     //relacionamento com product
-    @Transient
+
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
 
     public Category(Long id, String name) {
